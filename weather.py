debb0d8723fa4c6eea71b4ccaec250d6486c6296
MrCliff <j_1.kallio@hotmail.com>
2021-05-26 17:02:47 +0300

    Start adding Azure connectivity

--- weather.py
+++ weather.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import os
 import time
 from datetime import tzinfo
 import smbus2
@@ -14,15 +15,28 @@ from uuid import uuid4
 from awscrt import io, mqtt
 from awsiot import mqtt_connection_builder
 
+# Azure specific
+# Python Device SDK for IoT Hub:
+# https://github.com/Azure/azure-iot-sdk-python
+from azure.iot.device import IoTHubDeviceClient, Message
+
 I2C_PORT = 1
 BME280_ADDRESS = 0x76
 
 MQTT_TOPIC_FORMAT = "dt/weather/{location}/{client_id}"
 
+# Azure specific
+# The device connection string to authenticate the device with your IoT hub.
+# Using the Azure CLI:
+# az iot hub device-identity show-connection-string --hub-name {your IoT Hub name} --device-id {your device id} --output table
+AZURE_CONNECTION_STRING = "HostName={iot_hub_name}.azure-devices.net;DeviceId={device_id};SharedAccessKey={primary_key}"
+AZURE_DEVICE_PRIMARY_KEY = os.getenv("AZURE_DEVICE_PRIMARY_KEY")
+
+
 def init_arg_parser():
     parser = argparse.ArgumentParser(description="Periodically measure some weather parameters and send " +
-                                     "them to cloud services using MQTT. Uses MQTT topic of form \"" +
-                                     MQTT_TOPIC_FORMAT + "\".")
+                                     "them to different cloud services using MQTT. Where applicable, uses " +
+                                     "MQTT topic of form \"" + MQTT_TOPIC_FORMAT + "\".")
 
     # AWS specific
     parser.add_argument('--endpoint', required=True, help="Your AWS IoT custom endpoint, not including a port. " +
@@ -36,11 +50,15 @@ def init_arg_parser():
                         help="Logging level.")
     parser.add_argument('--log-file', default="stderr",
                         help="Log file location. Use 'stdout' or 'stderr' for stdout or stderr.")
+    parser.add_argument('--client-id', default=str(uuid4()), help="Client ID for MQTT connection.")
+
+    # Azure specific
+    parser.add_argument('--azure-iot-hub-name', required=True, help="Name of the Azure IoT Hub to which connect.")
+    parser.add_argument('--azure-device-id', required=True, help="Azure device ID for MQTT connection.")
 
     # General
     parser.add_argument('--location', default="earth", help="Physical location of the weather sensor. This is used as " +
                         "part of the MQTT topic.")
-    parser.add_argument('--client-id', default=str(uuid4()), help="Client ID for MQTT connection.")
     parser.add_argument('--msg-freq', default=5, help="Message frequency. Number of seconds to wait between measurements.")
 
     return parser
@@ -116,6 +134,75 @@ class AWSClient():
             payload=message,
             qos=mqtt.QoS.AT_MOST_ONCE)
 # AWS specifics END
+
+# Azure specifics START
+class AzureClient():
+    '''Class for all Azure Cloud specific functionality.'''
+    # TODO: Vaihda print-kutsut oikeaksi lokitukseksi.
+    def __init__(self, args):
+        self.iot_hub_name = args.azure_iot_hub_name
+        self.device_id = args.azure_device_id
+        self.key = AZURE_DEVICE_PRIMARY_KEY
+        self.location = args.location
+        # self.log_file = args.log_file
+        
+        # io.init_logging(getattr(io.LogLevel, self.verbosity), self.log_file)
+
+        self.init_connection()
+
+
+    def init_connection(self):
+        # event_loop_group = io.EventLoopGroup(1)
+        # host_resolver = io.DefaultHostResolver(event_loop_group)
+        # client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+
+        # self.mqtt_connection = mqtt_connection_builder.mtls_from_path(
+        #     endpoint=self.endpoint,
+        #     cert_filepath=self.cert,
+        #     pri_key_filepath=self.key,
+        #     client_bootstrap=client_bootstrap,
+        #     ca_filepath=self.root_ca,
+        #     on_connection_interrupted=self.on_connection_interrupted,
+        #     on_connection_resumed=self.on_connection_resumed,
+        #     client_id=self.client_id,
+        #     clean_session=False,
+        #     keep_alive_secs=6)
+
+# TODO: siirrä seuraavat AzureClienttiin.
+def iothub_client_init():
+    # Create an IoT Hub client
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+    return client
+
+def iothub_client_telemetry_sample_run():
+
+    try:
+        client = iothub_client_init()
+        print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
+
+        while True:
+            # Build the message with simulated telemetry values.
+            temperature = TEMPERATURE + (random.random() * 15)
+            humidity = HUMIDITY + (random.random() * 20)
+            msg_txt_formatted = MSG_TXT.format(temperature=temperature, humidity=humidity)
+            message = Message(msg_txt_formatted)
+
+            # Add a custom application property to the message.
+            # An IoT hub can filter on these properties without access to the message body.
+            if temperature > 30:
+              message.custom_properties["temperatureAlert"] = "true"
+            else:
+              message.custom_properties["temperatureAlert"] = "false"
+
+            # Send the message.
+            print( "Sending message: {}".format(message) )
+            client.send_message(message)
+            print ( "Message successfully sent" )
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        print ( "IoTHubClient sample stopped" )
+# Azure specifics END
 
 
 def init_sensors():
