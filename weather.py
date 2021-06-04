ae1d3d7ae55603e2589e7907d523d67a92586542
MrCliff <j_1.kallio@hotmail.com>
2021-06-04 17:13:34 +0300

    Add AzureClient and start using it
    Differentiate also AWS related CLI arguments from Azure related and
    general arguments.

--- weather.py
+++ weather.py
@@ -12,7 +12,9 @@ import json
 from uuid import uuid4
 
 # AWS specific
+# API: https://awslabs.github.io/aws-crt-python/
 from awscrt import io, mqtt
+# API: https://aws.github.io/aws-iot-device-sdk-python-v2/index.html
 from awsiot import mqtt_connection_builder
 
 # Azure specific
@@ -39,18 +41,18 @@ def init_arg_parser():
                                      "MQTT topic of form \"" + MQTT_TOPIC_FORMAT + "\".")
 
     # AWS specific
-    parser.add_argument('--endpoint', required=True, help="Your AWS IoT custom endpoint, not including a port. " +
+    parser.add_argument('--aws-endpoint', required=True, help="Your AWS IoT custom endpoint, not including a port. " +
                         "Ex: \"abcd123456wxyz-ats.iot.eu-west-1.amazonaws.com\"")
-    parser.add_argument('--cert', required=True, help="File path to your client certificate, in PEM format.")
-    parser.add_argument('--key', required=True, help="File path to your private key, in PEM format.")
-    parser.add_argument('--root-ca', help="File path to root certificate authority, in PEM format. " +
+    parser.add_argument('--aws-cert', required=True, help="File path to your client certificate, in PEM format.")
+    parser.add_argument('--aws-key', required=True, help="File path to your private key, in PEM format.")
+    parser.add_argument('--aws-root-ca', help="File path to root certificate authority, in PEM format. " +
                         "Necessary if MQTT server uses a certificate that's not already in " +
                         "your trust store.")
-    parser.add_argument('--verbosity', choices=[x.name for x in io.LogLevel], default=io.LogLevel.NoLogs.name,
+    parser.add_argument('--aws-verbosity', choices=[x.name for x in io.LogLevel], default=io.LogLevel.NoLogs.name,
                         help="Logging level.")
-    parser.add_argument('--log-file', default="stderr",
+    parser.add_argument('--aws-log-file', default="stderr",
                         help="Log file location. Use 'stdout' or 'stderr' for stdout or stderr.")
-    parser.add_argument('--client-id', default=str(uuid4()), help="Client ID for MQTT connection.")
+    parser.add_argument('--aws-client-id', default=str(uuid4()), help="Client ID for MQTT connection.")
 
     # Azure specific
     parser.add_argument('--azure-iot-hub-name', required=True, help="Name of the Azure IoT Hub to which connect.")
@@ -69,14 +71,13 @@ class AWSClient():
     '''Class for all AWS Cloud specific functionality.'''
     # TODO: Vaihda print-kutsut oikeaksi lokitukseksi.
     def __init__(self, args):
-        self.cert = args.cert
-        self.client_id = args.client_id
-        self.endpoint = args.endpoint
-        self.key = args.key
-        self.location = args.location
-        self.log_file = args.log_file
-        self.root_ca = args.root_ca
-        self.verbosity = args.verbosity
+        self.cert = args.aws_cert
+        self.client_id = args.aws_client_id
+        self.endpoint = args.aws_endpoint
+        self.key = args.aws_key
+        self.log_file = args.aws_log_file
+        self.root_ca = args.aws_root_ca
+        self.verbosity = args.aws_verbosity
         
         io.init_logging(getattr(io.LogLevel, self.verbosity), self.log_file)
 
@@ -121,10 +122,10 @@ class AWSClient():
 
 
     def disconnect(self):
-        print("Disconnecting...")
+        print("Disconnecting from AWS...")
         disconnect_future = self.mqtt_connection.disconnect()
         disconnect_future.result()
-        print("Disconnected!")
+        print("Disconnected from AWS!")
 
 
     def send_message(self, topic, message):
@@ -141,67 +142,37 @@ class AzureClient():
     # TODO: Vaihda print-kutsut oikeaksi lokitukseksi.
     def __init__(self, args):
         self.iot_hub_name = args.azure_iot_hub_name
+        self.hostname = args.azure_iot_hub_name + ".azure-devices.net"
         self.device_id = args.azure_device_id
         self.key = AZURE_DEVICE_PRIMARY_KEY
-        self.location = args.location
-        # self.log_file = args.log_file
-        
-        # io.init_logging(getattr(io.LogLevel, self.verbosity), self.log_file)
-
-        self.init_connection()
 
 
-    def init_connection(self):
-        # event_loop_group = io.EventLoopGroup(1)
-        # host_resolver = io.DefaultHostResolver(event_loop_group)
-        # client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+    def connect(self):
+        print("Connecting to host {} with device ID '{}'...".format(self.hostname, self.device_id))
+        formatted_conn_str = AZURE_CONNECTION_STRING.format(iot_hub_name=self.iot_hub_name,
+                                                            device_id=self.device_id,
+                                                            primary_key=self.key)
+        # self.client = IoTHubDeviceClient.create_from_connection_string(formatted_conn_str)
+        self.client = IoTHubDeviceClient.create_from_symmetric_key(self.key,
+                                                                   self.hostname,
+                                                                   self.device_id)
+        print("Connected!")
 
-        # self.mqtt_connection = mqtt_connection_builder.mtls_from_path(
-        #     endpoint=self.endpoint,
-        #     cert_filepath=self.cert,
-        #     pri_key_filepath=self.key,
-        #     client_bootstrap=client_bootstrap,
-        #     ca_filepath=self.root_ca,
-        #     on_connection_interrupted=self.on_connection_interrupted,
-        #     on_connection_resumed=self.on_connection_resumed,
-        #     client_id=self.client_id,
-        #     clean_session=False,
-        #     keep_alive_secs=6)
 
-# TODO: siirrä seuraavat AzureClienttiin.
-def iothub_client_init():
-    # Create an IoT Hub client
-    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
-    return client
+    def disconnect(self):
+        print("Disconnecting from Azure...")
+        self.client.disconnect()
+        print("Disconnected from Azure!")
 
-def iothub_client_telemetry_sample_run():
 
-    try:
-        client = iothub_client_init()
-        print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
+    def send_message(self, custom_properties, json_message):
+        message = Message(json_message, content_encoding="utf-8", content_type="application/json")
+        message.custom_properties.update(custom_properties)
+        # message.content_encoding = "utf-8"
+        # message.content_type = "application/json"
 
-        while True:
-            # Build the message with simulated telemetry values.
-            temperature = TEMPERATURE + (random.random() * 15)
-            humidity = HUMIDITY + (random.random() * 20)
-            msg_txt_formatted = MSG_TXT.format(temperature=temperature, humidity=humidity)
-            message = Message(msg_txt_formatted)
-
-            # Add a custom application property to the message.
-            # An IoT hub can filter on these properties without access to the message body.
-            if temperature > 30:
-              message.custom_properties["temperatureAlert"] = "true"
-            else:
-              message.custom_properties["temperatureAlert"] = "false"
-
-            # Send the message.
-            print( "Sending message: {}".format(message) )
-            client.send_message(message)
-            print ( "Message successfully sent" )
-            time.sleep(1)
-
-    except KeyboardInterrupt:
-        print ( "IoTHubClient sample stopped" )
+        print("Sending message to Azure: {}".format(message))
+        self.client.send_message(message)
 # Azure specifics END
 
 
@@ -222,16 +193,18 @@ def print_data(args, data):
     
     # there is a handy string representation too
     print(data)
-    print(to_json(args.client_id, data))
+    # print(to_json(args.client_id, data))
 
 
-def to_json(client_id, bme280data):
+def to_json(device_id, bme280data):
     '''Formats the given data into a JSON string.'''
+    utc_timestamp = round(bme280data.timestamp.astimezone().timestamp() * 1000) # UTC timestamp in milliseconds
     return json.dumps(
         {
-            "device_id": client_id,
+            "id": str(utc_timestamp), # Row key for Azure
+            "device_id": device_id,   # Partition key for AWS and Azure
             # "timestamp": bme280data.timestamp.astimezone().isoformat(), # Human readable timestamp
-            "timestamp": round(bme280data.timestamp.astimezone().timestamp() * 1000), # UTC timestamp in milliseconds
+            "timestamp": utc_timestamp, # Sort key for AWS
             "temperature": {
                 "value": round(bme280data.temperature, 2), # Resolution: 0.01°C
                 "unit": '°C'
@@ -248,7 +221,7 @@ def to_json(client_id, bme280data):
     )
 
 
-def send_data_to_aws(aws, data):
+def send_data_to_aws(args, aws, data):
     '''Sends the given data to AWS using the given AWSClient. The AWSClient
     should have connection open.
 
@@ -256,7 +229,15 @@ def send_data_to_aws(aws, data):
     "dt/weather/<sensor-location>/<sensor-client-id>"
 
     '''
-    aws.send_message(MQTT_TOPIC_FORMAT.format(location=aws.location, client_id=aws.client_id), to_json(aws.client_id, data))
+    aws.send_message(MQTT_TOPIC_FORMAT.format(location=args.location, client_id=aws.client_id), to_json(aws.client_id, data))
+
+
+def send_data_to_azure(args, azure, data):
+    '''Sends the given data to Azure using the given AzureClient. The AzureClient
+    should have connection open.
+
+    '''
+    azure.send_message({"location": args.location}, to_json(azure.device_id, data))
 
 
 def main(args):
@@ -265,18 +246,22 @@ def main(args):
 
     aws = AWSClient(args)
     aws.connect()
-
+    azure = AzureClient(args)
+    azure.connect()
+    
     try:
         while True:
             data = bme280.sample(bus, BME280_ADDRESS, calibration_params)
             print_data(args, data)
-            send_data_to_aws(aws, data)
+            # send_data_to_aws(args, aws, data)
+            send_data_to_azure(args, azure, data)
             
             time.sleep(args.msg_freq)
     except KeyboardInterrupt:
-        pass
+        print("Stopping...")
     finally:
         aws.disconnect()
+        azure.disconnect()
 
     print("Stopped")
 
